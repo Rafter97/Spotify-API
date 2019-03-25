756ac0172f8d57f581bfbc4a45f6a041e9432c85
Mark Rafter <S00181793@mail.itsligo.ie>
2019-03-25 15:59:27 +0000

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
